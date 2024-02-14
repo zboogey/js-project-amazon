f427180e0e551325acb91332dd4df9cb09ba766d
zboogey <ybraio8@gmail.com>
2024-02-14 20:59:44 +0600

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,14 @@ document.querySelectorAll('.js-add-to-card')
                 });
             }
 
-            console.log(cart);
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
+
         });
     });
